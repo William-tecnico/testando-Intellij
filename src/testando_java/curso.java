3e32fa2743bc0c0b5433c998cc5cbc78a512806f
William_técnico <wil.dossantos@gmail.com>
2022-01-04 16:24:56 -0300

    testando intellij

---
+++ src/testando_java/curso.java
@@ -0,0 +1,12 @@
+public class curso {
+
+    public static void main(String[] args) {
+        int b = 15;
+        int a = 5;
+
+        System.out.println(b + a);
+        System.out.println(a);
+    }
+
+}
+
