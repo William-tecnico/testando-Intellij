7e61792cde8c63be878cc147f691b04f9d557951
William_técnico <wil.dossantos@gmail.com>
2022-01-05 13:46:32 -0300

    testando controle de dados

--- src/testando_java/curso.java
+++ src/testando_java/curso.java
@@ -1,12 +1,36 @@
 public class curso {
 
     public static void main(String[] args) {
-        int b = 15;
-        int a = 5;
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 10000;
+        short s2 = 20000;
+
+        int i1 = 25;
+        int i2 = 505;
+
+        long l1 = 123456789L;
+        long l2 = 123456789l;
+
+        float f1 = 987654321f;
+        float f2 = 23546851F;
+
+        double d1 = 451d;
+        double d2 = 154d;
+
+        char c1 = 'A';
+        char c2 = 'B';
+
+        String s1 = "aula de string";
+        String s2 = "string 2";
+
+        boolean bo1 = true;
+        boolean bo2 = false
 
         //System.out.println(b + a);
         //System.out.println(a);
-        System.out.println("soma de 15 + 5: " + (a+b));
+        //System.out.println("soma de 15 + 5: " + (a+b));
 
     }
 }
